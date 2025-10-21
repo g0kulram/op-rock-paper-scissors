86fc7b477c9dab1a9ec5e319d88b4e6163ae9ed3
Gokul Ram <gokulram.ramesh@redingtongroup.com>
2025-10-21 14:04:48 +0530

    Implement getComputerChoice function
    Using the logic of three parts of 1 to each choice in RCP.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello!!!")
+console.log("Hello!!! Welcome to RCP!");
+
+//returns rock, paper or scissors randomly
+function getComputerChoice() {
+    const result = Math.random();
+
+    if(result >= 0 && result < (1/3)) { // in the range 0 <= result < 1/3
+        return("rock");
+    } else if(result >= (1/3) && result < (2/3)) { // in the range of 1/3 <= result < 2/3
+        return("paper");
+    } else { // in the range of 2/3 <= result <= 1(or 3/3)
+        return("scissors");
+    }
+}
+
